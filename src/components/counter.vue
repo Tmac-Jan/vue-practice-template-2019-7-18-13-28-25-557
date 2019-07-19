485d94590b4a4c1692cfe9a1f84a84d3a39968da
zhanggi3 <gio.zhang@oocl.com>
2019-07-19 17:45:31 +0800

    feat:counter

---
+++ src/components/counter.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="hello">
+
+<p>输入数字</p>
+  <button @click="inc" >add</button>
+  <span>result:{{count}}</span>
+  <button @click="dec" >dec</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'counter',
+  data(){
+  return{
+   count:0,
+   sum:0
+  }
+  },
+   components: {
+  },
+  props: {
+  },
+  computed: {
+
+  		},
+  methods: {
+  			//增加
+  			inc() {
+  				//const count = this.count
+  				this.count++;
+  			},
+  			//减少
+  			dec() {
+  				this.count--;
+        }
+  		}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
